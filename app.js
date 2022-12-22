6ae04dc2708525b449aa5e03fafce8550412d6e4
El-Farolito <lautimotoe99@gmail.com>
2022-12-21 21:12:38 -0300

    clase 22

--- app.js
+++ app.js
@@ -16,3 +16,6 @@ app.get('/', function(req, res){
 app.get('/register', function(req, res){
     res.sendFile(path.join(__dirname, "./views/register.html"))
 })
+app.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+})
