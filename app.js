22a30133fb7d6f9740b7b7f64188aca31db8a12f
El-Farolito <lautimotoe99@gmail.com>
2023-01-02 20:59:49 -0300

    modifique el PORT para render

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express");
 app = express();
 const path = require("path")
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
 const PORT = process.env.PORT || 3000
 
